38b2f4b4c1abde7a3e0114bbbba9d0769b079cd2
Andreas <34283919+eandmsz@users.noreply.github.com>
2026-09-09 10:53:04 +0200

    Update config.rs

--- core/src/config.rs
+++ core/src/config.rs
@@ -12,7 +12,7 @@
 //! GNU General Public License for more details.
 //!
 //! You should have received a copy of the GNU General Public License
-//! along with this program.  If not, see <https://www.gnu.org/licenses/>."
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 //! Persistent configuration. Every runtime knob the user can tweak
 //! either through the settings panel or by hand-editing
