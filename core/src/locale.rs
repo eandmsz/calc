f06a491b54f210d726b0272f05b8af1f4fdb26f1
Andreas <34283919+eandmsz@users.noreply.github.com>
2026-09-09 11:16:36 +0200

    Update locale.rs

--- core/src/locale.rs
+++ core/src/locale.rs
@@ -1,3 +1,16 @@
+//! Calc - An easy-to-use stateful scientific calculator focusing on intuitive user experience
+//! Copyright (C) 2027  Andras Vinter
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+
 //! Locale-aware defaults. The spec calls for the calculator's default
 //! decimal separator to match the OS locale – `','` for most of
 //! continental Europe, `'.'` everywhere else. We query
