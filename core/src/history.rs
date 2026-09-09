518400ff5228471952a3937488efcc9372cc8c14
Andreas <34283919+eandmsz@users.noreply.github.com>
2026-09-09 10:53:42 +0200

    Update history.rs

--- core/src/history.rs
+++ core/src/history.rs
@@ -1,3 +1,19 @@
+//! Calc - An easy-to-use stateful scientific calculator focusing on intuitive user experience
+//! Copyright (C) 2027  Andras Vinter
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 //! Evaluated-expression history with FIFO eviction.
 //!
 //! The side panel lists every completed calculation (input + result)
