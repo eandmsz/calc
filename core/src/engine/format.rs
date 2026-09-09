32428c41faf8c1579cdec2fb1a73a2b26734d25e
Andreas <34283919+eandmsz@users.noreply.github.com>
2026-09-09 11:20:01 +0200

    Update format.rs

--- core/src/engine/format.rs
+++ core/src/engine/format.rs
@@ -1,3 +1,16 @@
+//! Calc - An easy-to-use stateful scientific calculator focusing on intuitive user experience
+//! Copyright (C) 2027  Andras Vinter
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+
 //! Render a [`Decimal`] for the main display. Rounds to the configured
 //! number of *significant* digits, strips redundant zeroes, and
 //! switches to scientific notation when the value is very large or
