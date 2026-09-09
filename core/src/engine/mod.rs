538e0a7cae8ee0f33094d8090abc0de72fa57ad8
Andreas <34283919+eandmsz@users.noreply.github.com>
2026-09-09 11:21:17 +0200

    Update mod.rs

--- core/src/engine/mod.rs
+++ core/src/engine/mod.rs
@@ -1,3 +1,16 @@
+//! Calc - An easy-to-use stateful scientific calculator focusing on intuitive user experience
+//! Copyright (C) 2027  Andras Vinter
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+
 //! Engine module root. Re-exports the public surface that the UI and
 //! tests consume and defines the top-level Engine struct.
 
